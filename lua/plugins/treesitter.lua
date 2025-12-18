b00a701b85c6549bdbe80b56244c01f17531e5fa
paguso <paguso@cin.ufpe.br>
2025-12-18 00:15:45 -0300

    treesitter setup

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,14 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	lazy = false,
 	build = ":TSUpdate",
-    config = function ()
-		local configs = require("nvim-treesitter.configs")
-		configs.setup({
+	config = function()
+		require("nvim-treesitter").setup({
 			-- ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python", "javascript", "html", "markdown" },
 			auto_install = true,
 			sync_install = false,
 			highlight = { enable = true },
 			indent = { enable = true },
-        })
-    end
- }
+		})
+	end,
+}
