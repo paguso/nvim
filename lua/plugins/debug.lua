00c226c7ed7dd2b4954a147a1260002d9bf34136
paguso <paguso@cin.ufpe.br>
2025-12-21 23:23:52 -0300

    codelldgb, lazygit, telescope version

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -37,16 +37,28 @@ return {
 				},
 			} ]]
 
+			--[[
 			dap.adapters.lldb = {
 				type = "executable",
 				command = "/usr/bin/lldb-vscode-14",
 				name = "lldb",
 			}
+			]]
+
+			dap.adapters.codelldb = {
+				name = "codelldb",
+				type = "server",
+				port = "${port}",
+				executable = {
+					command = "codelldb",
+					args = { "--port", "${port}" },
+				},
+			}
 
 			dap.configurations.c = {
 				{
 					name = "Launch",
-					type = "lldb",
+					type = "codelldb",
 					request = "launch",
 					program = function()
 						return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
@@ -70,10 +82,24 @@ return {
 				},
 			}
 
+			dap.configurations.cpp = {
+				{
+					name = "Launch",
+					type = "codelldb",
+					request = "launch",
+					program = function()
+						return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+					end,
+					cwd = "${workspaceFolder}",
+					stopOnEntry = false,
+					args = {},
+				},
+			}
+
 			dap.configurations.rust = {
 				{
 					name = "Launch",
-					type = "lldb",
+					type = "codelldb",
 					request = "launch",
 					program = function()
 						return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,7 @@
 -- plugins/telescope.lua:
 return {
 	"nvim-telescope/telescope.nvim",
-	tag = "0.1.5",
+	tag = "v0.2.0",
 	-- or                              , branch = '0.1.x',
 	dependencies = {
 		"nvim-lua/plenary.nvim",
