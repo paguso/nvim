9b08957c0cfdf19264d7e197457da6cceb61174c
Paulo Fonseca <paguso@cin.ufpe.br>
2026-09-06 11:36:08 -0300

    fix telekasten loading

--- lua/plugins/telekasten.lua
+++ lua/plugins/telekasten.lua
@@ -1,19 +1,3 @@
--- Launch panel if nothing is typed after <leader>z
-vim.keymap.set("n", "<leader>z", "<cmd>Telekasten panel<CR>")
-
--- Most used functions
-vim.keymap.set("n", "<leader>zf", "<cmd>Telekasten find_notes<CR>")
-vim.keymap.set("n", "<leader>zg", "<cmd>Telekasten search_notes<CR>")
-vim.keymap.set("n", "<leader>zd", "<cmd>Telekasten goto_today<CR>")
-vim.keymap.set("n", "<leader>zz", "<cmd>Telekasten follow_link<CR>")
-vim.keymap.set("n", "<leader>zn", "<cmd>Telekasten new_note<CR>")
-vim.keymap.set("n", "<leader>zc", "<cmd>Telekasten show_calendar<CR>")
-vim.keymap.set("n", "<leader>zb", "<cmd>Telekasten show_backlinks<CR>")
-vim.keymap.set("n", "<leader>zI", "<cmd>Telekasten insert_img_link<CR>")
-
--- Call insert link automatically when we start typing a link
-vim.keymap.set("i", "[[", "<cmd>Telekasten insert_link<CR>")
-
 return {
 	"nvim-telekasten/telekasten.nvim",
 	dependencies = {
@@ -21,23 +5,26 @@ return {
 		"nvim-telescope/telescope.nvim",
 		"nvim-telekasten/calendar-vim"
 	},
-	ft = { 'markdown', 'telekasten' },
+	cmd = { "Telekasten" }, -- load on first Telekasten command
 	keys = {
-		{ "<leader>zt", "<cmd>Telekasten show_tags<CR>",   desc = "Search Telekasten Tags" },
-		{ "<leader>zf", "<cmd>Telekasten follow_link<CR>", desc = "Follow Link/Tag under cursor" },
+		{ "<leader>z",  "<cmd>Telekasten panel<CR>",           desc = "Telekasten Panel" },
+		{ "<leader>zf", "<cmd>Telekasten find_notes<CR>",      desc = "Find Notes" },
+		{ "<leader>zg", "<cmd>Telekasten search_notes<CR>",    desc = "Search Notes" },
+		{ "<leader>zd", "<cmd>Telekasten goto_today<CR>",      desc = "Go to Today" },
+		{ "<leader>zz", "<cmd>Telekasten follow_link<CR>",     desc = "Follow Link" },
+		{ "<leader>zn", "<cmd>Telekasten new_note<CR>",        desc = "New Note" },
+		{ "<leader>zc", "<cmd>Telekasten show_calendar<CR>",   desc = "Show Calendar" },
+		{ "<leader>zb", "<cmd>Telekasten show_backlinks<CR>",  desc = "Show Backlinks" },
+		{ "<leader>zI", "<cmd>Telekasten insert_img_link<CR>", desc = "Insert Image Link" },
+		{ "<leader>zt", "<cmd>Telekasten show_tags<CR>",       desc = "Search Tags" },
 	},
 	opts = {
-		--config = function()
-		--		require("telekasten").setup({
-		home = vim.fn.expand("~/zettelkasten"),
-		dailies = vim.fn.expand("~/zettelkasten/daily"),
-		extension = ".md",
-		new_note_filename = "title",
-		tag_notation = "#tag", -- how tags look in files
-		template_new_note = vim.fn.expand("~/zettelkasten/system/template.md"),
+		home               = vim.fn.expand("~/zettelkasten"),
+		dailies            = vim.fn.expand("~/zettelkasten/daily"),
+		extension          = ".md",
+		new_note_filename  = "title",
+		tag_notation       = "#tag",
+		template_new_note  = vim.fn.expand("~/zettelkasten/system/template.md"),
 		template_new_daily = vim.fn.expand("~/zettelkasten/system/daily.md"),
-		--		})
-		--end,
 	},
-	--}
 }
